bdab701db5d5afde8bbe923711bfc4c72c1e1bcb
Will Harr <103976107+will-harr@users.noreply.github.com>
2022-11-18 17:31:47 -0600

    Update account.py

--- account.py
+++ account.py
@@ -1,25 +1,25 @@
 class Account:
     def __init__(self, name: str):
-        self.account_name = name
-        self.account_balance = 0
+        self.__account_name = name
+        self.__account_balance = 0
 
     def deposit(self, amount: float):
         if amount > 0:
-            self.account_balance += amount
+            self.__account_balance += amount
             return True
 
         else:
             return False
 
     def withdraw(self, amount: float):
-        if (amount > 0) and (amount <= self.account_balance):
-            self.account_balance -= amount
+        if (amount > 0) and (amount <= self.__account_balance):
+            self.__account_balance -= amount
             return True
         else:
             return False
 
     def get_balance(self):
-        return self.account_balance
+        return self.__account_balance
 
     def get_name(self):
-        return self.account_name
+        return self.__account_name
